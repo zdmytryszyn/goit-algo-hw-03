5ccbb72a408a3779d604f15498a7913abc8ced0b
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-10-03 13:19:26 +0200

    Homework files

--- task_no1.py
+++ task_no1.py
@@ -6,8 +6,9 @@ def get_days_from_today(date: str) -> int | str:
 
     if type(date) is str:
         try:
-            today = datetime.today()
-            date_in_datetime_format = datetime.strptime(date, "%Y-%m-%d")
+
+            today = datetime.today().date()
+            date_in_datetime_format = datetime.strptime(date, "%Y-%m-%d").date()
             return (date_in_datetime_format - today).days
         except ValueError:
             return f"Wrong date format given: input in form \"{date}\" does not match format \"%Y-%m-%d\""
---
+++ task_no2.py
@@ -0,0 +1,10 @@
+# HOMEWORK - 2nd task
+import random
+
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int] | str:
+    if 1 <= min < max <= 1000 and (max - min) >= quantity:
+        lottery_numbers = sorted(set([random.randint(min, max) for i in range(quantity)]))
+        return f"Your lottery numbers: {lottery_numbers}" if len(lottery_numbers) == quantity \
+            else get_numbers_ticket(min, max, quantity)
+    return []
