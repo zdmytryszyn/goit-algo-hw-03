9344321308a5d5becd62c81188c7710e96396733
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-10-03 13:21:59 +0200

    Homework files

--- task_no1.py
+++ task_no1.py
@@ -6,7 +6,6 @@ def get_days_from_today(date: str) -> int | str:
 
     if type(date) is str:
         try:
-
             today = datetime.today().date()
             date_in_datetime_format = datetime.strptime(date, "%Y-%m-%d").date()
             return (date_in_datetime_format - today).days
